9a135cd99671a2111b9a66d29bae29cdc92b6d15
Michele Zanotti <m.zanotti@myti.it>
2020-12-05 17:40:22 +0100

    Add day_1 part two

--- day_1/main.py
+++ day_1/main.py
@@ -18,7 +18,20 @@ def get_two_entries_matching_sum(entries, target_sum):
     return None
 
 
-def main():
+def get_three_entries_matching_sum(entries, target_sum):
+    for i in range(0, len(entries)):
+        for j in range(i + 1, len(entries)):
+            for k in range(j + 1, len(entries)):
+                first_entry = entries[i]
+                second_entry = entries[j]
+                third_entry = entries[k]
+                entries_sum = first_entry + second_entry + third_entry
+                if entries_sum == target_sum:
+                    return first_entry, second_entry, third_entry
+    return None
+
+
+def main_part_one():
     entries = load_entries()
     result = get_two_entries_matching_sum(entries, ENTRIES_TARGET_SUM)
     if result is not None:
@@ -29,5 +42,17 @@ def main():
         print(f'Could not find entries summing up to {ENTRIES_TARGET_SUM}')
 
 
+def main_part_two():
+    entries = load_entries()
+    result = get_three_entries_matching_sum(entries, ENTRIES_TARGET_SUM)
+    if result is not None:
+        first_entry, second_entry, third_entry = result
+        print(f'Found entries ({first_entry},{second_entry},{third_entry}) that sum up to {ENTRIES_TARGET_SUM}')
+        print(f'{first_entry} * {second_entry} * {third_entry} = {first_entry * second_entry * third_entry}')
+    else:
+        print(f'Could not find entries summing up to {ENTRIES_TARGET_SUM}')
+
+
 if __name__ == '__main__':
-    main()
+    main_part_one()
+    main_part_two()
