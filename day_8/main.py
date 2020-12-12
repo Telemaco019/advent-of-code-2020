a5e63dd33d341940789b543c2689d74d991aa37a
Michele Zanotti <m.zanotti@myti.it>
2020-12-12 11:16:47 +0100

    Implement day_8 part 1

--- day_8/main.py
+++ day_8/main.py
@@ -18,41 +18,65 @@ class ExecutionState:
     def __init__(self, instruction_list: list):
         self.accumulator = 0
         self.instruction_list = instruction_list
+        self.current_instruction_index = 0
 
 
 class Instruction(metaclass=ABCMeta):
-    def __init__(self):
+    def __init__(self, argument):
         self.executions_count = 0
+        self.argument_sign = argument[0]
+        self.argument_value = int(argument[1:])
 
     @abstractmethod
-    def execute(self, state: ExecutionState) -> ExecutionState:
-        self.executions_count += 1
+    def execute(self, state: ExecutionState):
+        pass
 
 
 class ChangeAccumulatorInstruction(Instruction):
     def __init__(self, argument):
-        super().__init__()
+        super().__init__(argument)
 
-    def execute(self, state: ExecutionState) -> ExecutionState:
-        super().execute(state)
+    def execute(self, state: ExecutionState):
+        self.executions_count += 1
+        if self.argument_sign == '+':
+            state.accumulator += self.argument_value
+        elif self.argument_sign == '-':
+            state.accumulator -= self.argument_value
 
 
 class NoOpInstruction(Instruction):
     def __init__(self, argument):
-        super().__init__()
+        super().__init__(argument)
 
-    def execute(self, state: ExecutionState) -> ExecutionState:
-        super().execute(state)
+    def execute(self, state: ExecutionState):
+        self.executions_count += 1
 
 
 class JumpInstruction(Instruction):
     def __init__(self, argument):
-        super().__init__()
+        super().__init__(argument)
         self.argument = argument
 
-    def execute(self, state: ExecutionState) -> ExecutionState:
-        super().execute(state)
-        pass
+    def execute(self, state: ExecutionState):
+        self.executions_count += 1
+        if self.argument_sign == '+':
+            state.current_instruction_index += self.argument_value
+        elif self.argument_sign == '-':
+            state.current_instruction_index -= self.argument_value
+
+
+class Executor:
+    def __init__(self, execution_state: ExecutionState):
+        self.execution_state = execution_state
+
+    def run(self):
+        index = self.execution_state.current_instruction_index
+        instruction = self.execution_state.instruction_list[index]
+        if instruction.executions_count == 0:
+            instruction.execute(self.execution_state)
+            if self.execution_state.current_instruction_index == index:
+                self.execution_state.current_instruction_index += 1
+            self.run()
 
 
 class InstructionFactory:
@@ -76,9 +100,25 @@ def load_instructions(file_name):
     return [InstructionFactory.from_instruction_name(*line.split(' ')) for line in lines]
 
 
-def main():
-    instruction_list = load_instructions(TEST_INPUT_FILE_NAME)
+class PartOneTest(unittest.TestCase):
+    def test_count(self):
+        instruction_list = load_instructions(TEST_INPUT_FILE_NAME)
+        state = ExecutionState(instruction_list)
+        executor = Executor(state)
+        executor.run()
+        self.assertEqual(state.accumulator, 5)
+
+
+def part_one():
+    instruction_list = load_instructions(INPUT_FILE_NAME)
     state = ExecutionState(instruction_list)
+    executor = Executor(state)
+    executor.run()
+    print(f'The value of the accumulator is {state.accumulator}')
+
+
+def main():
+    part_one()
 
 
 if __name__ == '__main__':
